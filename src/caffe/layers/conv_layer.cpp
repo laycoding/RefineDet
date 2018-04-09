12883696a97340888be0e3b9a26fcf47f0bcc9fe
huajianni666 <35731605+huajianni666@users.noreply.github.com>
2018-04-09 22:16:00 +0800

    modefied

--- src/caffe/layers/conv_layer.cpp
+++ src/caffe/layers/conv_layer.cpp
@@ -27,6 +27,7 @@ void ConvolutionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* weight = this->blobs_[0]->cpu_data();
   for (int i = 0; i < bottom.size(); ++i) {
     const Dtype* bottom_data = bottom[i]->cpu_data();
+    LOG(INFO)<<"conv: "<<bottom_data[0]<<" "<< bottom_data[1];
     Dtype* top_data = top[i]->mutable_cpu_data();
     for (int n = 0; n < this->num_; ++n) {
       this->forward_cpu_gemm(bottom_data + n * this->bottom_dim_, weight,
