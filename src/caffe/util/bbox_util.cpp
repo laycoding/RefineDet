fdd231cc9bab1661d0e2a7c8d251bd223a281ba7
huajianni666 <35731605+huajianni666@users.noreply.github.com>
2018-04-10 09:53:55 +0800

    bbox

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -2627,7 +2627,7 @@ void VisualizeBBox(const vector<cv::Mat>& images, const Blob<Dtype>* detections,
                   CV_RGB(255, 255, 255), CV_FILLED);
     cv::putText(image, buffer, cv::Point(0, text.height + baseline / 2.),
                 fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
-
+    /*
     //生成供MTCNN用的正样本负样本，及对对应的人脸框。
     for (map<int, vector<NormalizedBBox> >::iterator it =
                      all_detections[i].begin(); it != all_detections[i].end(); ++it)
@@ -2706,7 +2706,7 @@ void VisualizeBBox(const vector<cv::Mat>& images, const Blob<Dtype>* detections,
                   }
     }
     count++;
-    /*
+    */
     // 选择保存WIDERFACE的每张测试图的结果TXT，同时可选择保存结果图。
     for (map<int, vector<NormalizedBBox> >::iterator it =
          all_detections[i].begin(); it != all_detections[i].end(); ++it)
@@ -2810,7 +2810,6 @@ void VisualizeBBox(const vector<cv::Mat>& images, const Blob<Dtype>* detections,
 	      cv::imwrite(fileName1,image);
     }
     count++;
-    */
   }
   start_clock = clock();
 }
