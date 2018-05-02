3940023d0057df6077c98e4e1ccbaf7bd273395d
huajianni666 <35731605+huajianni666@users.noreply.github.com>
2018-05-02 20:25:40 +0800

    mtcnn

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -2628,7 +2628,7 @@ void VisualizeBBox(const vector<cv::Mat>& images, const Blob<Dtype>* detections,
     cv::putText(image, buffer, cv::Point(0, text.height + baseline / 2.),
                 fontface, scale, CV_RGB(0, 0, 0), thickness, 8);
                 */
-    ##################################################
+    /*********************************************************/
     //生成供MTCNN用的正样本负样本，及对对应的人脸框。
     for (map<int, vector<NormalizedBBox> >::iterator it =
                      all_detections[i].begin(); it != all_detections[i].end(); ++it)
